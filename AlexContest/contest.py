218b28ab6472fefd8cf22112507bf2a0fc5285fe
manishas053 <manishas053@gmail.com>
2017-12-08 17:27:35 +0530

    Alex

--- AlexContest/contest.py
+++ AlexContest/contest.py
@@ -10,30 +10,29 @@
 
 input = raw_input()
 strLength = len(input)
+count = 0
 for i in range(0, strLength - 1):
-    #count = 0
-    if i + 5 < strLength:
+    if i + 5 <= strLength:
         if (input[i] == 'D' and input[i+1] == 'a' and input[i+2] == 'n' and input[i+3] == 'i' and input[i+4] == 'l'):
-            count = count + 1
-            print count
-    if i + 4 < strLength:
+            count += 1
+
+    if i + 4 <= strLength:
         if (input[i] == 'O' and input[i+1] == 'l' and input[i+2] == 'y' and input[i+3] == 'a'):
-            count = count + 1
-            print count
+            count += 1
 
-    if i + 5 < strLength:
-        if (input[i] == 'S' and input[i+1] == 'l' and input[i+2] == 'a' and input[i+3] == 'v' and input[i+4] == 'l'):
-            count = count + 1
+    if i + 5 <= strLength:
+        if (input[i] == 'S' and input[i+1] == 'l' and input[i+2] == 'a' and input[i+3] == 'v' and input[i+4] == 'a'):
+            count += 1
 
-    if i + 3 < strLength:
+    if i + 3 <= strLength:
         if (input[i] == 'A' and input[i+1] == 'n' and input[i+2] == 'n'):
-            count = count + 1
+            count += 1
 
-    if i + 5 < strLength:
+    if i + 6 <= strLength:
         if (input[i] == 'N' and input[i+1] == 'i' and input[i+2] == 'k' and input[i+3] == 'i' and input[i+4] == 't' and input[i+5] == 'a'):
-            count = count + 1
-print count
-#if count == 1:
-#    print "YES"
-#else:
-#    print "NO"
+            count += 1
+
+if count == 1:
+    print "YES"
+else:
+    print "NO"
